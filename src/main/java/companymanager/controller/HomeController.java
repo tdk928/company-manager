adb00c7d01f90734991254c16e0f3ed0469e3088
Teodor Kalev <teodorkalev@192.168.1.8>
2025-08-06 15:41:36 +0300

    Refactor package structure to flat 'companymanager' for custom project

--- src/main/java/companymanager/controller/HomeController.java
+++ src/main/java/companymanager/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.company.companymanager.controller;
+package companymanager.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
